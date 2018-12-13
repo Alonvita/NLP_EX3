327f6cbf138f65bd865642a5f00a0252b2c06ed6
alon.vita@gmail.com <zebra1zebra2!>
2018-12-13 15:16:43 +0200

    Initializing NLP ex3 created AssociationStrategy as a template for the 3 strategies to be created later on
    started working on SentenceAssociationStrategy

---
+++ SentenceAssociationStrategy.py
@@ -0,0 +1,8 @@
+import AssociationStrategy
+
+
+class SentenceAssociationStrategy(AssociationStrategy):
+    def __init__(self):
+        super.__init__()
+
+    def init_count_data_structures(self, fp, context_type):
